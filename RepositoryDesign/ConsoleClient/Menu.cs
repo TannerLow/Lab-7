937d0556aaa9b3d24a9dbd584888b401a72a2ed9
TannerLow <lowthorpt@gmail.com>
2020-10-26 14:25:04 -0700

    up to date

--- RepositoryDesign/ConsoleClient/Menu.cs
+++ RepositoryDesign/ConsoleClient/Menu.cs
@@ -9,16 +9,16 @@ namespace ConsoleClient
         /// </summary>
         public static void displayMenu()
         {
-            Console.WriteLine("[1] add a new teacher");
-            Console.WriteLine("[2] update a teacher");
-            Console.WriteLine("[3] remove a teacher");
-            Console.WriteLine("[4] list all teachers");
-            Console.WriteLine("[5] list all courses of a teacher");
-            Console.WriteLine("[6] add a new course");
-            Console.WriteLine("[7] update a course");
-            Console.WriteLine("[8] remove a course");
-            Console.WriteLine("[9] list all courses");
-            Console.WriteLine("[0] exit");
+            Console.WriteLine("1.) Create teacher");
+            Console.WriteLine("2.) Update teacher");
+            Console.WriteLine("3.) Delete teacher");
+            Console.WriteLine("4.) list teachers");
+            Console.WriteLine("5.) list courses of a teacher");
+            Console.WriteLine("6.) Create course");
+            Console.WriteLine("7.) Update course");
+            Console.WriteLine("8.) Delete course");
+            Console.WriteLine("9.) list courses");
+            Console.WriteLine("0.) quit");
         }
 
         /// <summary>
@@ -26,8 +26,8 @@ namespace ConsoleClient
         /// </summary>
         public static void clearMenu()
         {
-            Console.Clear();
-            Console.WriteLine("--------------------------------------------");
+            //Console.Clear();
+            Console.WriteLine("\n\n\n--------------------------------------------");
         }
 
         /// <summary>
@@ -35,11 +35,11 @@ namespace ConsoleClient
         /// </summary>
         public static void displaySearchOptions(bool updatingCourse = false)
         {
-            Console.WriteLine("[1] find by name");
-            Console.WriteLine("[2] find by id");
+            Console.WriteLine("1.) Find with name");
+            Console.WriteLine("2.) Find with ID");
             if (updatingCourse)
             {
-                Console.WriteLine("[3] find by Teacher id");
+                Console.WriteLine("3.) Find by Teacher ID");
             }
         }
     }
